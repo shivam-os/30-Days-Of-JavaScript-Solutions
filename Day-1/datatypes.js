dadad87c6f311b8cdc64014a6d7003499b4fa6d2
Shivam <shivam89raj45@gmail.com>
2022-09-03 22:45:52 +0530

    Create datatypes.js

---
+++ Day-1/datatypes.js
@@ -0,0 +1,41 @@
+//6. Declare four variables without assigning values 
+let myName;
+let yourName;
+let myAddress;
+let mySkills;
+
+
+//7. Declare four variables with assigned values
+let language = "JavaScript";
+let isEasy = false;
+let day = 1;
+let myNumber = 123456789;
+
+
+/* 5. Create datatypes.js file and use the JavaScript typeof operator to check different data types.
+Check the data type of each variable
+*/
+console.log(typeof language);
+console.log(typeof isEasy);
+console.log(typeof day);
+console.log(typeof myNumber);
+
+
+//8. Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let firstName;
+let lastName;
+let martialStatus;
+let country;
+let age;
+
+
+//9. Declare variables to store your first name, last name, marital status, country and age in a single line
+let first_name, last_name, martial_status, _country, _age
+
+
+//10. Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+let myAge = 23;
+let yourAge = 25;
+console.log("I am", myAge, "years old.");
+console.log("You are", yourAge, "years old.");
+
