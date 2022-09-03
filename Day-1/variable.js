194cd7e282078b239a7d6f05afd2bf03d8c8c93d
Shivam <shivam89raj45@gmail.com>
2022-09-03 22:32:30 +0530

    Create variable.js file

---
+++ Day-1/variable.js
@@ -0,0 +1,13 @@
+//comments can make code readable
+//Welcome to 30DaysOfJavaScript
+
+/*
+comments can make code readable,
+easy to reuse and informative
+*/
+
+let country = "India";
+let isPrime = false;
+let myChoice; //undefined by default
+let useless = null;
+
