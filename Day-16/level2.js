5ea3d70d775a51cfb82318702e2851b1c07a0e7c
Shivam <shivam89raj45@gmail.com>
2022-09-23 21:29:33 +0530

    Finish level 2

---
+++ Day-16/level2.js
@@ -0,0 +1,12 @@
+//1. Stringify the students object with only firstName, lastName and skills properties
+
+const student = {
+  firstName:'Asabeneh',
+  lastName:'Yetayehe',
+  age:250,
+  isMarried:true,
+  skills:['HTML', 'CSS', 'JS', 'React','Node', 'Python', ]
+}
+
+let studentJSON = JSON.stringify(student, ["firstName", "lastName", "skills"], 4);
+console.log(studentJSON);
