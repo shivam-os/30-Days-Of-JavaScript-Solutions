bc03aa4d3ea8fa7afdbddd22d1309c86b8ebb7f2
Shivam <shivam89raj45@gmail.com>
2022-09-23 20:22:00 +0530

    Finish level 2

---
+++ Day-13/level2.js
@@ -0,0 +1,10 @@
+//1. 10 > 2 * 10 use console.assert()
+console.assert(10 > 2 * 10, "is greater than 2 * 10");
+
+
+//2. Write a warning message using console.warn()
+console.warn("Update your JS engine to the latest version for better support across the new JS syntax.");
+
+
+//3. Write an error message using console.error()
+console.error("Result can't be computed.")
